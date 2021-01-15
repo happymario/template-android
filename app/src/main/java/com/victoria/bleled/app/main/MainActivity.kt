502e69b37fc8afec50c024ebd89474c1e8e201b1
happymario <kaankoca97@gmail.com>
2021-01-16 00:03:10 +0800

    complete main Structure

--- app/src/main/java/com/victoria/bleled/app/main/MainActivity.kt
+++ app/src/main/java/com/victoria/bleled/app/main/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.victoria.bleled.R
+import com.victoria.bleled.app.main.mode.MonthCalendarFragment
 import com.victoria.bleled.app.test.TestActivity
+import com.victoria.bleled.util.CommonUtil
 
 class MainActivity : AppCompatActivity() {
     /************************************************************
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     /************************************************************
      *  UI controls & Data members
      ************************************************************/
-
+    private var fragmentMonthCalendar : MonthCalendarFragment = MonthCalendarFragment()
 
     /************************************************************
      *  Overrides
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initView()
     }
 
 
@@ -41,9 +44,27 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun onMenu(view: View) {
+        CommonUtil.showNIToast(this)
+    }
+
     /************************************************************
      *  Helpers
      ************************************************************/
+    private fun initView() {
+        // view
+        initFragment()
+
+        // event
+
+    }
+
+    private fun initFragment() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fl_main_content, fragmentMonthCalendar)
+        fragmentTransaction.commit()
+    }
 
 
     /************************************************************
