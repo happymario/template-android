d9bb1bede9a28d329700153feb94dc61a73f56e3
DESKTOP-Q8DS6SU\happymario <kaankoca97@gmail.com>
2022-12-15 22:52:51 +0800

    add initializer

--- app/src/main/java/com/victoria/bleled/app/MyApplication.kt
+++ app/src/main/java/com/victoria/bleled/app/MyApplication.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDex
 import com.android.volley.Request
 import com.android.volley.RequestQueue
@@ -58,6 +59,9 @@ class MyApplication : Application() {
      ************************************************************/
     override fun onCreate() {
         super.onCreate()
+
+        // Non-Dark Theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         instance = this
 
